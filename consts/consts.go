3a751331781d306338d2077aa399c89278a53c27
BOLUUUU7657 <suleimanboluwaife@gmail.com>
2024-07-02 14:59:14 +0000

    bavalanche-hypersdk

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Boluwatife"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Bolu"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "B"
 )
 
 var ID ids.ID
